fcdeb9bb31d23b4afd3630f0d054988a29692e9e
Ghinwa213 <bahij@uwindsor.ca>
2025-11-20 21:38:10 -0500

    Add Test Cases for empty files

--- Testinglhdiff.java
+++ Testinglhdiff.java
@@ -2,6 +2,7 @@ package lhdiftests;
 
 import static org.junit.Assert.*;
 
+import javax.xml.transform.Result;
 
 import org.junit.Test;
 
@@ -10,7 +11,6 @@ public class Testinglhdiff {
 
 @Test
 public void testIdenticalFiles() {
-
 String[]left= {"A","B","C"};
 String[]right= {"A","B","C"};
 	
@@ -22,12 +22,20 @@ assertEquals(1, result.getMapping(1));
 assertEquals(2, result.getMapping (2));
 assertEquals(3, result.getMapping (3));
 
-
+}
 	
-//@Test
-//public void testEmptyFiles()
+@Test
+public void testEmptyFiles() {
+	String[] left= {};
+	String[] right= {};
 	
 	
+	//TODO:replace with the call of actual LHDIFF
+	//LHDiffResult result = LHDiff.compute(left, right);
+		
+	//Expected no Mapping, both files are empty
+	assertTrue(result.isempty());
+}
 //@Test
 //public void testSingleLineAdditionAtEnd()
 	
